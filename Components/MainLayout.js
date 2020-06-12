676e22f19b0a7ca12762283045936285d4e3a992
Darkkillerxxx <adidabs1994@gmail.com>
2020-06-12 18:00:09 +0530

    Setting Up Login and Register

---
+++ Components/MainLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View } from 'react-native'
+import MWisrNavigator from '../Navigator/Navigator'
+
+class MainLayout extends React.Component{
+    constructor()
+    {
+        super()
+    }
+
+    render()
+    {
+        return(
+            <MWisrNavigator />
+        )
+    }
+}
+
+export default MainLayout
