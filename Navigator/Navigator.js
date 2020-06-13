c039cc01d3728dea232863a04e4bb3f8bb4289ea
Darkkillerxxx <adidabs1994@gmail.com>
2020-06-13 19:57:08 +0530

    Setting Up Onboarding

--- Navigator/Navigator.js
+++ Navigator/Navigator.js
@@ -1,7 +1,10 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import Login from '../Screens/Login'
-import Register from '../Screens/Register'
+import Login from '../Screens/Login/Login'
+import Register from '../Screens/Register/Register'
+import Home from '../Screens/Home/Home'
+import OTP from '../Screens/OTP/OTP'
+import Onboarding from '../Screens/Onboarding/Onboarding'
 
 const MwisrNavigation=createStackNavigator({
     Login:{
@@ -9,6 +12,15 @@ const MwisrNavigation=createStackNavigator({
     },
     Register:{
         screen:Register
+    },
+    Home:{
+        screen:Home
+    },
+    OTP:{
+        screen:OTP
+    },
+    Onboarding:{
+        screen:Onboarding
     }
 },{
     headerMode:'none'
