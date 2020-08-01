6640eb1150695422564f8a88246244f7752c80b3
Darkkillerxxx <adidabs1994@gmail.com>
2020-08-01 14:13:16 +0530

    Assign Package Stated

--- Components/ReportFilter.js
+++ Components/ReportFilter.js
@@ -10,18 +10,79 @@ class ReportFilter extends React.Component{
     {
         super();
         this.state={
-            Owners:[
-                {
-                "OwnerId": "",
-                "OwnerName": "Own",
-                "SuperOwner": ""
-                }
-            ],
             SearchText:"",
             SearchTag:"",
-            Exchange:[],
-            CallStatus:0,
-            OwnerId:""
+            SelectedMarketCap:null,
+            MarketCaps:[
+                {
+                    Id:null,
+                    Name:"All"
+                },
+                {
+                    Id:1,
+                    Name:"Large Cap"
+                },
+                {
+                    Id:2,
+                    Name:"Medium Cap"
+                },
+                {
+                    Id:3,
+                    Name:"Small Cap"
+                }
+            ],
+            ReportType:[
+                {
+                    Id:null,
+                    Name:"All"
+                },
+                {
+                    Id:1,
+                    Name:"Technical"
+                },
+                {
+                    Id:2,
+                    Name:"Fundamental"
+                },
+                {
+                    Id:3,
+                    Name:"TechnoFunda"
+                }
+            ],
+            SegmentType:[
+                {
+                    Id:null,
+                    Name:"All"
+                },
+                {
+                    Id:1,
+                    Name:"Equity"
+                },
+                {
+                    Id:2,
+                    Name:"EquityFutures"
+                },
+                {
+                    Id:3,
+                    Name:"EquityOptions"
+                },
+                {
+                    Id:4,
+                    Name:"CommodityFutures"
+                },
+                {
+                    Id:5,
+                    Name:"CommodityOptions"
+                },
+                {
+                    Id:6,
+                    Name:"CurrencyFutures"
+                },
+                {
+                    Id:7,
+                    Name:"CurrencyOptions"
+                }
+            ]
         }
     }
 
@@ -64,6 +125,36 @@ class ReportFilter extends React.Component{
     CallStatus=(status)=>{
         this.setState({CallStatus:status})
     }
+
+    ShowMarketCaps=(itemData)=>{
+        return(
+            <View style={{width:'32%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 0 ? '#F0B22A':'white'}`,marginTop:5,marginRight:5}}>
+                <TouchableOpacity onPress={()=>this.CallStatus(0)}>
+                    <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 0 ? 'white':'#F0B22A'}`}}>{itemData.item.Name}</NormalText>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+     ShowReports=(itemData)=>{
+        return(
+            <View style={{width:'32%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 0 ? '#F0B22A':'white'}`,marginTop:5,marginRight:5}}>
+                <TouchableOpacity onPress={()=>this.CallStatus(0)}>
+                    <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 0 ? 'white':'#F0B22A'}`}}>{itemData.item.Name}</NormalText>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+
+    ShowSegments=(itemData)=>{
+        return(
+            <View style={{width:'32%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 0 ? '#F0B22A':'white'}`,marginTop:5,marginRight:5}}>
+                <TouchableOpacity onPress={()=>this.CallStatus(0)}>
+                    <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 0 ? 'white':'#F0B22A'}`}}>{itemData.item.Name}</NormalText>
+                </TouchableOpacity>
+            </View>
+        )
+    }
     
     render()
     {
@@ -72,7 +163,7 @@ class ReportFilter extends React.Component{
             <View style={styles.CallsFilterContainer}>
                 <View style={styles.FilterHeadingContainer}>
                     <BoldText style={styles.FilterHeading}>Filters</BoldText>
-                    <TouchableOpacity onPress={()=>this.props.closeFilter(SearchText,Exchange,CallStatus,OwnerId)}>
+                    <TouchableOpacity onPress={()=>this.props.closeFilter(SearchText)}>
                         <View style={styles.FilterButton}>
                             <FontAwesome name="filter" size={24} color="white" />
                             <NormalText style={{marginBottom:0,color:'white'}}>Apply Filters</NormalText> 
@@ -81,7 +172,17 @@ class ReportFilter extends React.Component{
                 </View>
 
                 <View style={{width:'100%',marginVertical:5}}>
-                    <BoldText style={{fontSize:15}}>Search</BoldText>
+                    <BoldText style={{fontSize:15}}>Search By Name</BoldText>
+                        <View style={{width:'100%',alignItems:'center'}}>
+                            <View style={{width:'90%',height:40,borderWidth:1,borderColor:'#bcc4cb',flexDirection:'row',alignItems:'center',borderRadius:5}}>
+                                <TextInput onChangeText={(e)=>this.setState({SearchText:e})} style={{width:'90%',height:37,backgroundColor:'white'}} underlineColorAndroid ='transparent'/>
+                                <TouchableOpacity>
+                                    <FontAwesome name="search" size={24} color="#bcc4cb" />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                        <BoldText style={{fontSize:15}}>Search By Tag</BoldText>
                         <View style={{width:'100%',alignItems:'center'}}>
                             <View style={{width:'90%',height:40,borderWidth:1,borderColor:'#bcc4cb',flexDirection:'row',alignItems:'center',borderRadius:5}}>
                                 <TextInput onChangeText={(e)=>this.setState({SearchText:e})} style={{width:'90%',height:37,backgroundColor:'white'}} underlineColorAndroid ='transparent'/>
@@ -91,68 +192,34 @@ class ReportFilter extends React.Component{
                             </View>
                         </View>
                 </View>
-
-                <View style={{width:'100%',marginVertical:5}}>
-                    <BoldText style={{fontSize:15}}>Exchanges</BoldText>
-                    <View style={{width:'100%',flexDirection:'row',justifyContent:'space-around'}}>
-                        <View style={{width:'18%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${Exchange.length === 0 ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.SelectExchange("")}>
-                                <NormalText style={{color:'black',marginBottom:0,color:`${Exchange.length === 0 ? 'white':'#F0B22A'}`}}>All</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'18%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${Exchange.includes('NSE') ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.SelectExchange('NSE')}>
-                                <NormalText style={{marginBottom:0,color:`${Exchange.includes('NSE') ? 'white':'#F0B22A'}`}}>NSE</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'18%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${Exchange.includes('BSE') ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.SelectExchange('BSE')}>
-                                <NormalText style={{marginBottom:0,color:`${Exchange.includes('BSE') ? 'white':'#F0B22A'}`}}>BSE</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'18%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${Exchange.includes('NCDEX') ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.SelectExchange('NCDEX')}>
-                                <NormalText style={{color:`${Exchange.includes('NCDEX') ? 'white':'#F0B22A'}`,marginBottom:0}}>NCDEX</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'18%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${Exchange.includes('MCX') ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.SelectExchange('MCX')}>
-                                <NormalText style={{marginBottom:0,color:`${Exchange.includes('MCX') ? 'white':'#F0B22A'}`}}>MCX</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
                 
-                <View style={{width:'100%',marginVertical:5}}>
-                    <BoldText style={{fontSize:15}}>Call Status</BoldText>
-                    <View style={{width:'100%',flexDirection:'row',justifyContent:'space-around'}}>
-                        <View style={{width:'30%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 0 ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.CallStatus(0)}>
-                                <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 0 ? 'white':'#F0B22A'}`}}>All Calls</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'30%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 1 ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.CallStatus(1)}>
-                                <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 1 ? 'white':'#F0B22A'}`}}>Active Calls</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                        <View style={{width:'30%',borderWidth:1,alignItems:'center',justifyContent:'center',padding:5,borderRadius:5,borderColor:'#F0B22A',backgroundColor:`${this.state.CallStatus === 2 ? '#F0B22A':'white'}`}}>
-                            <TouchableOpacity onPress={()=>this.CallStatus(2)}>
-                                <NormalText style={{marginBottom:0,color:`${this.state.CallStatus === 2 ? 'white':'#F0B22A'}`}}>In-Active Calls</NormalText>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-
-                <View style={{width:'100%',marginVertical:5}}>
-                    <BoldText style={{fontSize:15}}>Owners</BoldText>
+               <View style={{width:'100%',marginVertical:5}}>
+                    <BoldText style={{fontSize:15}}>Market Cap</BoldText>
                     <FlatList
-                        keyExtractor={(item, index) => item.OwnerId}
-                        data={this.state.Owners}
-                        renderItem={this.showOwners}
-                        numColumns={3}/>                 
+                        keyExtractor={(item,index)=>index.toString()}
+                        data={this.state.MarketCaps}
+                        renderItem={this.ShowMarketCaps} 
+                        numColumns={3}/>
                 </View>
-                
+
+                <View style={{width:'100%',marginVertical:5}}>
+                    <BoldText style={{fontSize:15}}>Report Type</BoldText>
+                    <FlatList
+                        keyExtractor={(item,index)=>index.toString()}
+                        data={this.state.ReportType}
+                        renderItem={this.ShowReports} 
+                        numColumns={3}/>
+                </View>
+
+                <View style={{width:'100%',marginVertical:5}}>
+                    <BoldText style={{fontSize:15}}>Segment Type</BoldText>
+                    <FlatList
+                        keyExtractor={(item,index)=>index.toString()}
+                        data={this.state.SegmentType}
+                        renderItem={this.ShowSegments} 
+                        numColumns={3}/>
+                </View>
+             
                 
             </View>
         )
